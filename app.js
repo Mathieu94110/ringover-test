e4372be86e4a0106cfd622975813f23e0f3270b5
Mathieu Enault <me94110@gmail.com>
2023-03-24 09:35:53 +0100

    preparing requests for http calls, get and post works, and refactoring app logic in order to get the input value and the possibility to update this value when item checkbox input is checked

--- app.js
+++ app.js
@@ -1,80 +1,220 @@
+// api
+const baseUrl = "http://127.0.0.1:9000/v1/tasks";
+
+const getTasks = async () => {
+  try {
+    const response = await fetch(baseUrl);
+    console.log("res", response);
+    if (response.status > 400) {
+      return alert(response.statusText);
+    } else {
+      const jsonRes = await response.json();
+      return jsonRes;
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+const updateTask = async (taskInfos) => {
+  try {
+    const response = await fetch(`${baseUrl}/${taskInfos.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(taskInfos),
+    });
+    console.log("res", response);
+    if (response.status == 409) {
+      return alert(response.statusText);
+    } else {
+      alert("Tâche mise à jour !");
+    }
+    return jsonRes;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const data = {
+  label: "labeltask22",
+  description: "tasks",
+  start_date: "2022-01-01T12:00:00Z", // inputt  type='date'
+  end_date: "",
+};
+
+const postTasks = async (taskInfos) => {
+  try {
+    const response = await fetch(baseUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(taskInfos),
+    });
+    console.log("res", response);
+    if (response.status == 409) {
+      return alert(response.statusText);
+    } else {
+      alert("Tâche crée !");
+    }
+    return jsonRes;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const deleteTask = async (label) => {
+  try {
+    const response = await fetch(`${baseUrl}/${label}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(taskInfos),
+    });
+    console.log("res", response);
+    if (response.status == 409) {
+      return alert(response.statusText);
+    } else {
+      alert("Tâche supprimée !");
+    }
+    return jsonRes;
+  } catch (error) {
+    throw error;
+  }
+};
+//
+
 const inpSearch = document.querySelector(".inpSearch");
 const addButton = document.querySelector(".add");
 const textArea = document.querySelector(".inp");
 const tasksList = document.querySelector(".ul");
+const tasksForm = document.getElementById("to-do-list-container");
 
-// Variables //
-const allTasks = [];
+async function main() {
+  let tasks = await getTasks();
+  console.log("tasks", tasks);
+  // let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-// Functions //
-function renderTaskList() {
-  const li = document.createElement("li");
-  const taskContent = document.createElement("input");
-  const deleteBtn = document.createElement("button");
-  const editBtn = document.createElement("button");
-
-  taskContent.setAttribute("readonly", "readonly");
-  taskContent.className = "task-content";
-  deleteBtn.classList.add("deleteBtn");
-  editBtn.classList.add("editBtn");
-  deleteBtn.innerHTML = "<i class='fa-solid fa-trash'></i>";
-  editBtn.innerHTML = "<i class='fa-solid fa-pen-to-square'></i>";
-
-  if (!(textArea.value === "")) {
-    allTasks.push(li);
-    tasksList.appendChild(li);
-    li.appendChild(taskContent);
-    li.appendChild(editBtn);
-    li.appendChild(deleteBtn);
-    taskContent.value = textArea.value;
-
-    allTasks.forEach(function (element, key) {
-      element.id = key;
-      tasksList.appendChild(element);
+  if (localStorage.getItem("tasks")) {
+    tasks.map((task) => {
+      createTask(task);
     });
-  } else {
-    return;
   }
 
-  textArea.value = "";
-  taskContent.disabled = true;
+  textArea.addEventListener("keydown", function (event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      const inputValue = textArea.value;
+      if (inputValue === "") {
+        return;
+      }
+      const task = {
+        id: new Date().getTime(),
+        description: inputValue,
+        isCompleted: false,
+      };
+      /*
+      model
+      const task = {
+          label: "labeltasks"
+        description: inputValue,
+     start_date:new Date().getTime(),
+             end_date://example: "2022-01-01T15:00:00Z"
+      };*/
 
-  editBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-    if (editBtn.firstChild.classList.contains("fa-pen-to-square")) {
-      taskContent.disabled = false;
-      taskContent.removeAttribute("readonly");
-      taskContent.focus();
-      editBtn.innerHTML = "<i class='fa-solid fa-check'></i>";
-    } else {
-      taskContent.setAttribute("readonly", "readonly");
-      taskContent.disabled = true;
-      editBtn.innerHTML = "<i class='fa-solid fa-pen-to-square'></i>";
+      tasks.push(task);
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+      createTask(task);
+      tasksForm.reset();
+      textArea.focus();
     }
   });
 
-  deleteBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-    const index = event.target.parentNode.id;
-    allTasks.splice(index, 1);
-    this.parentElement.remove();
+  function createTask(task) {
+    const taskEl = document.createElement("li");
+    taskEl.setAttribute("id", task.id);
+
+    if (task.isCompleted) {
+      taskEl.classList.add("complete");
+    }
+
+    const taskElMarkup = `
+  <div class="taskName">
+  <span ${!task.isCompleted ? "contenteditable" : ""} class="task-content">${
+      task.description
+    }</span>
+  <input type="checkbox" name="tasks" id="${task.id}" class="editBtn" ${
+      task.isCompleted ? "checked" : ""
+    }>
+  <button class="remove-task deleteBtn"><i class='fa-solid fa-trash'></i></button>
+  </div>
+  `;
+
+    taskEl.innerHTML = taskElMarkup;
+    tasksList.appendChild(taskEl);
+  }
+
+  tasksList.addEventListener("click", (e) => {
+    if (
+      e.target.classList.contains("remove-task") ||
+      e.target.parentElement.classList.contains("remove-task") ||
+      e.target.parentElement.parentElement.classList.contains("remove-task")
+    ) {
+      const taskId = e.target.closest("li").id;
+      removeTask(taskId);
+    }
+  });
+
+  function removeTask(taskId) {
+    console.log(taskId);
+    tasks = tasks.filter((task) => {
+      task.id !== parseInt(taskId);
+    });
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    document.getElementById(taskId).remove();
+  }
+
+  tasksList.addEventListener("input", (e) => {
+    const taskId = e.target.closest("li").id;
+    updateTask(taskId, e.target);
+  });
+
+  function updateTask(taskId, el) {
+    const task = tasks.find((task) => task.id === parseInt(taskId));
+    if (el.hasAttribute("contenteditable")) {
+      task.name = el.textContent;
+    } else {
+      // const span = el.nextElementSibling;
+      const span = el.previousElementSibling;
+      const parent = el.closest("li");
+
+      task.isCompleted = !task.isCompleted;
+
+      if (task.isCompleted) {
+        span.removeAttribute("contenteditable");
+        parent.classList.add("complete");
+      } else {
+        span.setAttribute("contenteditable", "true");
+        parent.classList.remove("complete");
+      }
+    }
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+
+  // filtered
+
+  tasksList.addEventListener("click", (e) => {
+    if (
+      e.target.classList.contains("remove-task") ||
+      e.target.parentElement.classList.contains("remove-task")
+    ) {
+      const taskId = e.target.closest("li").id;
+      removeTask(taskId);
+    }
   });
 }
 
-// Events //
-
-textArea.addEventListener("keydown", function (event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    renderTaskList();
-  }
-});
-
-addButton.addEventListener("click", function () {
-  event.preventDefault();
-  renderTaskList();
-});
-
-inpSearch.addEventListener("keyup", () => {
-  renderTaskList();
-});
+main();
