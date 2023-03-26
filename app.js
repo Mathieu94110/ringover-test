165dae9a775826b775c2c5c024255d0b8968913b
Mathieu Enault <me94110@gmail.com>
2023-03-26 13:35:39 +0200

    see task details and change end date inside dialog modal works, and code refactoring

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+const inpSearch = document.querySelector(".inpSearch");
+const tasksList = document.querySelector(".ul");
+const tasksDetails = document.getElementById("favDialog");
+const tasksForm = document.getElementById("to-do-list-form");
+const labelDetails = document.getElementById("label-details");
+const descriptionDetails = document.getElementById("description-details");
+const creationDetails = document.getElementById("creation-details");
+const endDetails = document.getElementById("end-details");
+const detailsBtn = document.getElementsByClassName("detailsBtn");
+const dateInput = document.getElementsByClassName("dateInput");
+const dateWrapper = document.getElementById("input-date-wrapper");
+
 // api
 const baseUrl = "http://127.0.0.1:9000/v1/tasks";
 
@@ -21,8 +33,7 @@ const getTask = async (label) => {
   try {
     const response = await fetch(`${baseUrl}/${label}`);
     if (response.status === 200) {
-      const jsonRes = await response.json();
-      return jsonRes;
+      return response;
     } else if (response.status === 201) {
       return alert("Aucun contenu correspondant !");
     } else if (response.status === 404) {
@@ -68,7 +79,7 @@ const deleteTask = async (label) => {
       },
     });
     if (response.status == 200) {
-      alert("Tâche supprimée !");
+      return response;
     } else if (response.status == 404) {
       return alert("Tâche non trouvée !");
     } else if (response.status === 500) {
@@ -89,7 +100,7 @@ const updateTask = async (taskInfos) => {
       body: JSON.stringify(taskInfos),
     });
     if (response.status == 200) {
-      return alert("La date de fin de la tâche a été mise à jour !");
+      return response;
     } else if (response.status == 400) {
       return alert("Mauvaise requete !");
     } else if (response.status == 404) {
@@ -102,9 +113,130 @@ const updateTask = async (taskInfos) => {
   }
 };
 
-const inpSearch = document.querySelector(".inpSearch");
-const tasksList = document.querySelector(".ul");
-const tasksForm = document.getElementById("to-do-list-container");
+async function addTasks(task) {
+  if (task && task.label) {
+    const taskEl = document.createElement("li");
+    taskEl.setAttribute("id", task.label);
+    if (task.isCompleted) {
+      taskEl.classList.add("complete");
+    }
+    const taskElMarkup = `
+  <div class="taskName">
+  <span  class="task-content">${task.description}</span>
+  <button onclick="" class="detailsBtn"  id="${
+    task.label
+  }"><i class="fa-solid fa-magnifying-glass"></i> </button>
+  <span  class="task-end">${
+    task.end_date ? formatDate(task.end_date) : "Pas de date de fin"
+  }</span>
+  <button onclick="removeTask(event)" id="${
+    task.label
+  }"class="deleteBtn"><i class='fa-solid fa-trash'></i></button>
+  </div>
+  `;
+    taskEl.innerHTML = taskElMarkup;
+    tasksList.appendChild(taskEl);
+  }
+}
+
+const removeTask = async (e) => {
+  let label;
+  if (e.target.id) {
+    label = e.target.id;
+  } else {
+    label = e.target.parentElement.id;
+  }
+  const response = await deleteTask(label);
+  if (response && response.ok) {
+    document.getElementById(label).remove();
+  }
+};
+
+const getTaskInfos = async (label) => {
+  const response = await getTask(label);
+  return response;
+};
+
+async function onOpen(label) {
+  const response = await getTaskInfos(label);
+  const jsonRes = await response.json();
+  if (response && response.ok) {
+    console.log(jsonRes);
+    const { label, description, end_date, start_date } = jsonRes;
+    const inputDetails = `
+      <label class="task-content">Changer la date de fin:</label>   
+      <input
+    type="date"
+    min="2023-03-24"
+    max="2030-12-31"
+    id="${label}"
+    name="end_date"
+    pattern="\d{4}-\d{2}-\d{2}"
+    class="dateInput"
+    onchange="changeEndDate(event)"
+    />
+    `;
+
+    labelDetails.innerHTML = label;
+    descriptionDetails.innerHTML = description;
+    creationDetails.innerHTML = formatDate(start_date);
+    endDetails.innerHTML = end_date ? end_date : "Pas de date de fin";
+    dateWrapper.innerHTML = inputDetails;
+    //
+    if (typeof favDialog.showModal === "function") {
+      favDialog.showModal();
+    } else {
+      console.error(
+        "L'API <dialog> n'est pas prise en charge par ce navigateur."
+      );
+    }
+  }
+}
+
+const changeEndDate = async (e) => {
+  console.log(e.target);
+  const label = e.target.id;
+  const isoDate = new Date(e.target.value).toISOString();
+  const response = await updateTask({
+    label: label,
+    end_date: isoDate,
+  });
+  if (response.ok) {
+    endDetails.innerHTML = formatDate(isoDate);
+    document
+      .getElementById(label)
+      .getElementsByClassName("task-end")[0].innerHTML = formatDate(isoDate);
+    alert("Date de fin modifiée !");
+  }
+};
+
+function closeTaskDetailsModal() {
+  favDialog.close();
+  tasksDetails.close();
+}
+
+function formatDate(isoDate) {
+  return isoDate.replace(/T.*/, "").split("-").reverse().join("-");
+}
+
+// Events
+
+tasksList.addEventListener("click", async (e) => {
+  console.log(e.target.parentElement.classList);
+  if (
+    e.target.classList.contains("detailsBtn") ||
+    e.target.parentElement.classList.contains("detailsBtn")
+  ) {
+    e.stopPropagation();
+    let label;
+    if (e.target.id) {
+      label = e.target.id;
+    } else {
+      label = e.target.parentElement.id;
+    }
+    onOpen(label);
+  }
+});
 
 tasksForm.addEventListener("submit", async (event) => {
   event.preventDefault();
@@ -116,110 +248,23 @@ tasksForm.addEventListener("submit", async (event) => {
     description: description,
     start_date: new Date().toISOString(),
   };
-
   const response = await postTask(newTask);
   if (response && response.ok) {
     tasksForm.reset();
     addTasks(newTask);
   }
 });
-
-async function addTasks(task) {
-  if (task && task.label) {
-    const taskEl = document.createElement("li");
-    taskEl.setAttribute("id", task.label);
-    if (task.isCompleted) {
-      taskEl.classList.add("complete");
-    }
-    const taskElMarkup = `
-  <div class="taskName">
-  <span  class="task-content">${task.description}</span>
-    <input
-    type="date"
-    min="2023-03-24"
-    max="2030-12-31"
-    id="end_date"
-    name="end_date"
-    pattern="\d{4}-\d{2}-\d{2}"
-    class="dateInput"
-    />
-  <button class="remove-task deleteBtn"><i class='fa-solid fa-trash'></i></button>
-  </div>
-  `;
-    taskEl.innerHTML = taskElMarkup;
-    tasksList.appendChild(taskEl);
-  }
-}
+//
 
 async function main() {
   let tasks = await getTasks();
+  console.log(tasks);
   if (tasks && tasks.length > 0) {
     tasks.map((task) => {
       addTasks(task);
     });
   }
-
   addTasks(tasks);
-
-  tasksList.addEventListener("click", (e) => {
-    if (
-      e.target.classList.contains("remove-task") ||
-      e.target.parentElement.classList.contains("remove-task") ||
-      e.target.parentElement.parentElement.classList.contains("remove-task")
-    ) {
-      const taskId = e.target.closest("li").id;
-      removeTask(taskId);
-    } else if (
-      e.target.classList.contains("dateInput") ||
-      e.target.parentElement.classList.contains("dateInput")
-    ) {
-      tasksList.querySelectorAll(".dateInput").forEach((inputElement) => {
-        const taskId = e.target.closest("li").id;
-        inputElement.addEventListener("change", (event) => {
-          updateTaskEndDate(event, taskId);
-        });
-      });
-    }
-  });
-
-  // detect when end_date is choosen or changed in tasks list
-
-  function updateTaskEndDate(event, taskId) {
-    const date = event.target.value;
-    if (date && taskId) {
-      const isoDate = new Date(date).toISOString();
-      const taskInfos = {
-        label: taskId,
-        end_date: isoDate,
-      };
-      updateTask(taskInfos);
-    }
-  }
-
-  function removeTask(taskId) {
-    tasks = tasks.filter((task) => {
-      task.id !== parseInt(taskId);
-    });
-    document.getElementById(taskId).remove();
-  }
-
-  tasksList.addEventListener("input", (e) => {
-    const taskId = e.target.closest("li").id;
-    updateTaskOnClient(taskId, e.target);
-  });
-
-  // filter Coming soon !!
-
-  tasksList.addEventListener("click", async (e) => {
-    if (
-      e.target.classList.contains("remove-task") ||
-      e.target.parentElement.classList.contains("remove-task")
-    ) {
-      e.preventDefault();
-      const taskId = e.target.closest("li").id;
-      await deleteTask(taskId);
-    }
-  });
 }
 
 main();
